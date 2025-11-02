5dfca0f44fcaf20135228dc6ba2e734c39662953
Tanveer-rajpurohit <tanveersinghrajpurohit4@gmail.com>
2025-11-02 11:24:17 +0530

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
       >
         <ThemeProvider 
           attribute="class" 
-          defaultTheme="light" 
+          defaultTheme="dark" 
           enableSystem={false}
           storageKey="remote-classroom-theme"
           disableTransitionOnChange={false}
